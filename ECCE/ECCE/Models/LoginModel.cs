b3a3496745ce77a684bcaea61f52655d749e8473
Daniel Toguti <danieltoguti@gmail.com>
2021-02-27 14:25:24 -0300

    Criação do Projeto

---
+++ ECCE/ECCE/Models/LoginModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ECCE.Models
+{
+    public class LoginModel
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Nome", Prompt ="")]
+        public string Nome { get; set; }
+    }
+}
