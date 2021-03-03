aca214b97eb58aa3e8a89c69234fc97174e6d642
Daniel Toguti <danieltoguti@gmail.com>
2021-03-03 12:45:08 -0300

    Crud Funcao

---
+++ ECCE/ECCE/Models/EnderecoModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ECCE.Models
+{
+    public class EnderecoModel
+    {
+        tb_endereco tb_endereco { get; set; }
+    }
+    
+    public class tb_endereco
+    {
+        public int CodigoEnderoco { get; set; }
+        public int CodigoLogin { get; set; }
+        public string Descricao { get; set; }
+        public string Cep { get; set; }
+        public string Endereco { get; set; }
+        public string Numero { get; set; }
+        public string Bairro { get; set; }
+        public string Cidade { get; set; }
+        public string UF { get; set; }
+
+    }
+
+}
--- ECCE/ECCE/Models/LoginModel.cs
+++ ECCE/ECCE/Models/LoginModel.cs
@@ -8,14 +8,45 @@ namespace ECCE.Models
 {
     public class LoginModel
     {
-        public int Id { get; set; }
+        tb_login tb_Login { get; set; }
+        List<tb_funcionario_funcao> tb_funcionario_funcao { get; set; }
+    }
+    public class tb_login
+    {
+        public int CodigoLogin { get; set; }
 
-        [Display(Name = "Nome", Prompt ="")]
+        [Display(Name = "Nome", Prompt = "")]
         public string Nome { get; set; }
 
+        [Display(Name = "Email", Prompt = "")]
         public string Email { get; set; }
+
+        [Display(Name = "Telefone", Prompt = "")]
+        public string Telefone { get; set; }
+
+        [Display(Name = "CPF/CNPJ", Prompt = "")]
+        public string CPF_CNPJ { get; set; }
+
+        [Display(Name = "CodigoFuncao", Prompt = "")]
+        public string CodigoFuncao { get; set; }
 
         [Display(Name = "Senha", Prompt = "")]
         public string Senha { get; set; }
+
+        [Display(Name = "Tipo", Prompt = "")]
+        public string Tipo { get; set; }
+
+        [Display(Name = "Data Cadastro", Prompt = "")]
+        public string DataCadastro { get; set; }
+
+        [Display(Name = "Ativo", Prompt = "")]
+        public string Ativo { get; set; }
     }
+
+    public class tb_funcionario_funcao
+    {
+        public int CodigoFuncao { get; set; }
+        public string Descricao { get; set; }
+    }
+
 }
